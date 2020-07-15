c2df08a090f3b074286a9d40af4ddcac2b42db90
yuwei <yuwei@yuweix.com>
2020-07-15 18:08:38 +0800

    1.0.49

--- assist4j-data/src/main/java/com/assist4j/data/cache/redis/jedis/JedisClusterCache.java
+++ assist4j-data/src/main/java/com/assist4j/data/cache/redis/jedis/JedisClusterCache.java
@@ -55,7 +55,7 @@ public class JedisClusterCache extends AbstractCache implements RedisCache {
 	}
 
 	@Override
-	public void subscribe(String channel, final MessageHandler handler) {
+	public void subscribe(final String channel, final MessageHandler handler) {
 		new Thread() {
 			@Override
 			public void run() {
@@ -134,7 +134,7 @@ public class JedisClusterCache extends AbstractCache implements RedisCache {
 			return resMap;
 		}
 		for (Map.Entry<String, String> strEntry: strMap.entrySet()) {
-			resMap.put(strEntry.getKey(), deserialize(strEntry.getValue()));
+			resMap.put(strEntry.getKey(), (T) deserialize(strEntry.getValue()));
 		}
 		return resMap;
 	}
@@ -204,7 +204,7 @@ public class JedisClusterCache extends AbstractCache implements RedisCache {
 			return tList;
 		}
 		for (String str: strList) {
-			tList.add(deserialize(str));
+			tList.add((T) deserialize(str));
 		}
 		return tList;
 	}
@@ -261,7 +261,7 @@ public class JedisClusterCache extends AbstractCache implements RedisCache {
 			return tSet;
 		}
 		for (String str: strSet) {
-			tSet.add(deserialize(str));
+			tSet.add((T) deserialize(str));
 		}
 		return tSet;
 	}
