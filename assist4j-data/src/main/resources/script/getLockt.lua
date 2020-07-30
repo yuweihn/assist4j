8d045fe62e799d655cf08aeede7a895537d15746
yuwei <yuwei@yuweix.com>
2020-07-30 19:26:56 +0800

    1.0.50

--- assist4j-data/src/main/resources/script/getLockt.lua
+++ assist4j-data/src/main/resources/script/getLockt.lua
@@ -1,24 +1,13 @@
-local res = nil
-if ARGV[1] == 'true' then
-    res = redis.call('get', KEYS[1])
-    if res == ARGV[2] then
-        local r = redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'XX')
-        if (r ~= 'OK' and r ~= 'ok') then
-            res = nil
-        end
+if redis.call('get', KEYS[1]) == ARGV[1] then
+    if redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'XX') == "OK" then
+        return ARGV[1];
     else
-        local r = redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'NX')
-        if (r == 'OK' or r == 'ok') then
-            res = ARGV[2]
-        end
+        return nil;
     end
 else
-    local r = redis.call('set', KEYS[1], ARGV[2], 'EX', ARGV[3], 'NX')
-    if (r == 'OK' or r == 'ok') then
-        res = ARGV[2]
+    if redis.call('set', KEYS[1], ARGV[1], 'EX', ARGV[2], 'NX') == "OK" then
+        return ARGV[1];
     else
-        res = redis.call('get', KEYS[1])
+        return redis.call('get', KEYS[1]);
     end
 end
-
-return res
