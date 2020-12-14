b30bfbeab2623edd1ab6f9c5560ca3bd12771a2c
yuwei <fisher.yu@agilenaas.com>
2020-12-14 18:40:22 +0800

    1.0.1

--- assist4j-dao/src/main/java/com/yuweix/assist4j/dao/mybatis/provider/AbstractProvider.java
+++ assist4j-dao/src/main/java/com/yuweix/assist4j/dao/mybatis/provider/AbstractProvider.java
@@ -230,4 +230,20 @@ public abstract class AbstractProvider {
 			throw new RuntimeException(e);
 		}
 	}
+
+	protected String getPhysicalTable(Class<?> entityClass, Object shardingVal) {
+		String tbName = getTableName(entityClass);
+		List<FieldColumn> fcList = getPersistFieldList(entityClass);
+		for (FieldColumn fc: fcList) {
+			Field field = fc.getField();
+			Sharding sharding = field.getAnnotation(Sharding.class);
+			if (sharding == null) {
+				continue;
+			}
+
+			String shardingIndex = getShardingIndex(sharding, tbName, shardingVal);
+			return tbName + "_" + shardingIndex;
+		}
+		return tbName;
+	}
 }
