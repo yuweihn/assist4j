4dca1e54ef31876505575e4c106539a5bdcedfc0
yuwei <yuwei@yuweix.com>
2020-12-18 13:27:42 +0800

    1.0.1

--- assist4j-session/src/main/java/com/yuweix/assist4j/session/filter/AbstractSessionFilter.java
+++ assist4j-session/src/main/java/com/yuweix/assist4j/session/filter/AbstractSessionFilter.java
@@ -2,8 +2,6 @@ package com.yuweix.assist4j.session.filter;
 
 
 import com.yuweix.assist4j.session.CacheHttpServletRequest;
-import com.yuweix.assist4j.session.CookiesUtil;
-import com.yuweix.assist4j.session.SessionConstant;
 import com.yuweix.assist4j.session.cache.SessionCache;
 import com.yuweix.assist4j.session.conf.PathPattern;
 import com.yuweix.assist4j.session.conf.SessionConf;
@@ -12,13 +10,12 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.UUID;
 
 
 /**
  * @author yuwei
  */
-public class AbstractSessionFilter implements Filter {
+public abstract class AbstractSessionFilter implements Filter {
 	/**
 	 * 需要排除的URI
 	 */
@@ -68,32 +65,7 @@ public class AbstractSessionFilter implements Filter {
 		after(cacheRequest, httpResponse);
 	}
 
-	protected String getSessionId(HttpServletRequest request, HttpServletResponse response) {
-		String sid = CookiesUtil.findValueByKey(request
-				, SessionConf.getInstance().getApplicationName() + SessionConstant.COOKIE_SESSION_ID_SUFFIX);
-		if (sid == null || "".equals(sid)) {
-			sid = generateSessionId(request, response);
-		}
-		return sid;
-	}
-
-	private String generateSessionId(HttpServletRequest request, HttpServletResponse response) {
-		String sid = UUID.randomUUID().toString().replace("-", "");
-		if (sid == null || "".equals(sid)) {
-			throw new RuntimeException("生成SessionId失败！！！");
-		}
-		addCookie(sid, request, response);
-		return sid;
-	}
-
-	/**
-	 * 更新cookie值
-	 */
-	private void addCookie(String sid, HttpServletRequest request, HttpServletResponse response) {
-		CookiesUtil.addCookie(request, response
-				, SessionConf.getInstance().getApplicationName() + SessionConstant.COOKIE_SESSION_ID_SUFFIX
-				, sid,null,null, SessionConstant.COOKIE_MAX_AGE_DEFAULT);
-	}
+	protected abstract String getSessionId(HttpServletRequest request, HttpServletResponse response);
 
 	protected void before(HttpServletRequest request, HttpServletResponse response) {
 
